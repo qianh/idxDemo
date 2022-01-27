bbb92824d4fa69a54b3118db2d450508dd5571d9
钱宏 <john@qianhongdeMacBook-Pro.local>
2022-01-27 17:57:59 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,7 @@ export default function() {
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-            测试功能231
+            开始测试
           </a>
         </li>
       </ul>
